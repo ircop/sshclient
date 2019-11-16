e72e62d5c2180b6feb88bba13a557ef13f25f4bc
Mikhail Samin <i@mishasamin.xyz>
2019-11-17 00:13:15 +0300

    Added ssh.Signer as an auth method

--- client.go
+++ client.go
@@ -3,45 +3,46 @@
 package sshclient
 
 import (
+	"bytes"
+	"fmt"
+	"github.com/google/goterm/term"
+	"github.com/pkg/errors"
+	"github.com/sasha-s/go-deadlock"
 	"golang.org/x/crypto/ssh"
 	"io"
+	"regexp"
 	"strings"
 	"time"
-	"fmt"
-	"github.com/pkg/errors"
-	"github.com/google/goterm/term"
-	"regexp"
-	"bytes"
-	"github.com/sasha-s/go-deadlock"
 )
 
 // callbackPattern struct handles callbacks that should be called when corresponding
 // regex matches.
 type callbackPattern struct {
-	Re			*regexp.Regexp
-	Cb			func()
-	SkipLine	bool
+	Re       *regexp.Regexp
+	Cb       func()
+	SkipLine bool
 }
 
 // SshClient is ssh client struct itself
 type SshClient struct {
-	User		string
-	Password	string
-	prompt		string
-	Timeout		int
-	TimeoutGlobal	int
+	User          string
+	Password      string
+	prompt        string
+	Timeout       int
+	TimeoutGlobal int
 
-	sshClient	*ssh.Client
-	sshSession	*ssh.Session
-	outPipe		io.Reader
-	inPipe		io.WriteCloser
-	errPipe		io.Reader
+	sshClient  *ssh.Client
+	sshSession *ssh.Session
+	sshSigner  *ssh.Signer
+	outPipe    io.Reader
+	inPipe     io.WriteCloser
+	errPipe    io.Reader
 
-	reading		bool
-	buf			bytes.Buffer
+	reading bool
+	buf     bytes.Buffer
 
-	patterns	[]callbackPattern
-	mx			deadlock.Mutex
+	patterns []callbackPattern
+	mx       deadlock.Mutex
 }
 
 // New returns new instance of SshClient. Arguments are: connection timeout, username, password, prompt.
@@ -52,10 +53,10 @@ func New(tout int, user string, password string, prompt string) *SshClient {
 	}
 
 	c := SshClient{
-		Timeout:tout,
-		User:user,
-		Password:password,
-		prompt: `(?msi:[\$%#>]$)`,
+		Timeout:  tout,
+		User:     user,
+		Password: password,
+		prompt:   `(?msi:[\$%#>]$)`,
 		patterns: make([]callbackPattern, 0),
 	}
 
@@ -82,6 +83,11 @@ func (c *SshClient) SetLogin(login string) {
 func (c *SshClient) SetPassword(pw string) {
 	c.mx.Lock()
 	c.Password = pw
+	c.mx.Unlock()
+}
+func (c *SshClient) SetSigner(signer *ssh.Signer) {
+	c.mx.Lock()
+	c.sshSigner = signer
 	c.mx.Unlock()
 }
 
@@ -122,22 +128,30 @@ func (c *SshClient) Open(host string, port int) error {
 	}
 
 	config := ssh.ClientConfig{
-		User:				c.User,
-		Auth:				[]ssh.AuthMethod{
-								ssh.PasswordCallback(func()(string, error){ return c.Password, nil }),
-								ssh.KeyboardInteractive(challenge),
-								ssh.Password(c.Password),
-							},
-		HostKeyCallback:	ssh.InsecureIgnoreHostKey(),
-		Timeout:			time.Second * time.Duration(c.TimeoutGlobal),
+		User:            c.User,
+		Auth:            []ssh.AuthMethod{},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Timeout:         time.Second * time.Duration(c.TimeoutGlobal),
+	}
+	if c.sshSigner != nil {
+		config.Auth = []ssh.AuthMethod{
+			ssh.PublicKeys(*c.sshSigner),
+		}
+	}
+	if c.Password != "" {
+		config.Auth = append(config.Auth,
+			ssh.PasswordCallback(func() (string, error) { return c.Password, nil }),
+			ssh.KeyboardInteractive(challenge),
+			ssh.Password(c.Password),
+		)
 	}
 	config.Ciphers = append(config.Ciphers, "aes128-ctr", "aes192-ctr", "aes256-ctr", "chacha20-poly1305@openssh.com", "aes128-gcm@openssh.com", "aes256-gcm@openssh.com")
 
-	c.sshClient, err = ssh.Dial("tcp", fmt.Sprintf("%s:%d", host, port), &config )
+	c.sshClient, err = ssh.Dial("tcp", fmt.Sprintf("%s:%d", host, port), &config)
 	if err != nil {
 		if strings.Contains(err.Error(), "no common algorithm") {
 			config.Ciphers = []string{"aes128-cbc", "3des-cbc", "aes192-cbc", "aes256-cbc"}
-			c.sshClient, err = ssh.Dial("tcp", fmt.Sprintf("%s:%d", host, port), &config )
+			c.sshClient, err = ssh.Dial("tcp", fmt.Sprintf("%s:%d", host, port), &config)
 			if err != nil {
 				return errors.Wrap(err, "SSH dial failed")
 			}
@@ -145,7 +159,6 @@ func (c *SshClient) Open(host string, port int) error {
 			return errors.Wrap(err, "SSH dial failed")
 		}
 	}
-
 
 	// Setup terminal
 	terminal := term.Termios{}
@@ -160,9 +173,9 @@ func (c *SshClient) Open(host string, port int) error {
 
 	// setup some session params
 	modes := ssh.TerminalModes{
-		ssh.ECHO:				0,
-		ssh.TTY_OP_ISPEED:		14400000,
-		ssh.TTY_OP_OSPEED:		14400000,
+		ssh.ECHO:          0,
+		ssh.TTY_OP_ISPEED: 14400000,
+		ssh.TTY_OP_OSPEED: 14400000,
 	}
 	if err = c.sshSession.RequestPty("xterm", 132, 100, modes); err != nil {
 		return errors.Wrap(err, "Failed to request SSH PTY")
@@ -182,13 +195,13 @@ func (c *SshClient) Open(host string, port int) error {
 	}
 
 	err = c.sshSession.Shell()
-	if err !=  nil {
+	if err != nil {
 		return errors.Wrap(err, "Failed to request ssh shell")
 	}
 
 	// Wait for prompt just after login
 	_, err = c.ReadUntil(c.prompt)
-	
+
 	return err
 }
 
@@ -202,8 +215,8 @@ func (c *SshClient) RegisterCallback(pattern string, callback func()) error {
 
 	c.mx.Lock()
 	c.patterns = append(c.patterns, callbackPattern{
-		Cb:callback,
-		Re:re,
+		Cb: callback,
+		Re: re,
 	})
 	c.mx.Unlock()
 
